3d6b65a7ffd3be91e87fe430be564f5a6f20e4b9
Audrey Man <82979811+MournTHEMorning@users.noreply.github.com>
2024-08-18 22:38:45 -0400

    Game Over UI screen
    Prefab and script made; conditions tbd

---
+++ Assets/Scripts/GameOverUIScript.cs
@@ -0,0 +1,42 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+
+public class GameOverUIScript : MonoBehaviour
+{
+    [SerializeField] private GameObject gameOverScreen;
+    [SerializeField] private TMP_Text titleText;
+    [SerializeField] private TMP_Text quotaText;
+    [SerializeField] private TMP_Text scoreText;
+    [SerializeField] private TMP_Text totalTimeText;
+
+    [Tooltip("Any side messages like 'new ingredients unlocked' or 'thanks for playing'")]
+    [SerializeField] private TMP_Text endText;
+
+
+    //takes the basic text for stats
+    private string quotaPrefix, scorePrefix,totalTimePrefix;
+    private void Awake()
+    {
+        Assert.IsNotNull(gameOverScreen);
+        Assert.IsNotNull(titleText);
+        Assert.IsNotNull(quotaText);
+        Assert.IsNotNull(scoreText);
+        Assert.IsNotNull(totalTimeText);
+        Assert.IsNotNull(endText);
+
+        quotaPrefix = quotaText.text;
+        scorePrefix = scoreText.text;
+        totalTimePrefix = totalTimeText.text;
+
+        gameOverScreen.SetActive(false);
+    }
+
+    public void OnEnable(){
+        //ref: waveText.text = wavePrefix + progressHandler.Wave;
+        Debug.Log("Oh hey!");
+
+    }
+
+}
