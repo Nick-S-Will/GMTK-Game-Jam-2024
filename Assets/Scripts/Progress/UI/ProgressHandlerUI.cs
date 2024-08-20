07d50890379a6dc9ef38f76af7f24b27e8f20919
Nicholas Williams <nickwill820@gmail.com>
2024-08-19 23:22:27 -0400

    Added squish and stretch to pets

--- Assets/Scripts/Progress/UI/ProgressHandlerUI.cs
+++ Assets/Scripts/Progress/UI/ProgressHandlerUI.cs
@@ -4,32 +4,28 @@ using UnityEngine.Assertions;
 
 public class ProgressHandlerUI : MonoBehaviour
 {
-
     [Header("Handlers")]
     [SerializeField] private ProgressHandler progressHandler;
     [SerializeField] private ScoreHandler scoreHandler;
-
 
     [Header("Text Fields")]
     [SerializeField] private TMP_Text waveText;
     [SerializeField] private TMP_Text timeText;
     [SerializeField] private TMP_Text scoreText;
 
-
     private string wavePrefix, timePrefix, scorePrefix;
 
     private void Awake()
     {
         Assert.IsNotNull(progressHandler);
+        Assert.IsNotNull(scoreHandler);
         Assert.IsNotNull(waveText);
         Assert.IsNotNull(timeText);
         Assert.IsNotNull(scoreText);
 
-
         wavePrefix = waveText.text;
         timePrefix = timeText.text;
         scorePrefix = scoreText.text;
-
     }
 
     private void Update()
